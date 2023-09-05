eb13db48e4fe0c7ad83f36943fbf437c15e43756
dlachaumepalo <dlachaumepalo@users.noreply.github.com>
2023-09-05 07:21:37 +0000

    deploy: 11aa3f4ae8dd4765f4de172409e8aca5105ae3f6

--- rust-doc/mithril_common/api_version/sidebar-items.js
+++ rust-doc/mithril_common/api_version/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["APIVersionProviderError"],"fn":["get_open_api_versions_mapping"],"struct":["APIVersionProvider"],"type":["OpenAPIFileName","OpenAPIVersionRaw"]};
+window.SIDEBAR_ITEMS = {"fn":["get_open_api_versions_mapping"],"struct":["APIVersionProvider"],"type":["OpenAPIFileName","OpenAPIVersionRaw"]};
