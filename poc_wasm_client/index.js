3e57c907ebcae68f2873724edffb8405e18c04a4
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-10-02 18:25:44 +0200

    Display certificate chain verification in browser

--- poc_wasm_client/index.js
+++ poc_wasm_client/index.js
@@ -1,3 +1,14 @@
-import { main } from './pkg';
+import { run } from './pkg';
 
-main();
+// preprod
+/* let certificate_hash = "61b241a842ae986e54df26a43f5ebef2c6876d1b6fba5122cb20ca77df74131f";
+let aggregator_endpoint = "https://aggregator.release-preprod.api.mithril.network/aggregator";
+let genesis_verification_key = "5b3132372c37332c3132342c3136312c362c3133372c3133312c3231332c3230372c3131372c3139382c38352c3137362c3139392c3136322c3234312c36382c3132332c3131392c3134352c31332c3233322c3234332c34392c3232392c322c3234392c3230352c3230352c33392c3233352c34345d";
+ */
+
+//mainnet
+let certificate_hash = "5e811dd8b17ace941a3b29fb8fc641469b7a48e91149f437e535a96c4d320222";
+let aggregator_endpoint = "https://aggregator.release-mainnet.api.mithril.network/aggregator";
+let genesis_verification_key = "5b3139312c36362c3134302c3138352c3133382c31312c3233372c3230372c3235302c3134342c32372c322c3138382c33302c31322c38312c3135352c3230342c31302c3137392c37352c32332c3133382c3139362c3231372c352c31342c32302c35372c37392c33392c3137365d";
+
+run(aggregator_endpoint, certificate_hash, genesis_verification_key);
--- poc_wasm_client/src/lib.rs
+++ poc_wasm_client/src/lib.rs
@@ -3,6 +3,7 @@ mod client;
 use wasm_bindgen::prelude::*;
 
 use client::*;
+use web_sys::Window;
 
 pub const GENESIS_VERIFICATION_KEY: &str = "5b3132372c37332c3132342c3136312c362c3133372c3133312c3231332c3230372c3131372c3139382c38352c3137362c3139392c3136322c3234312c36382c3132332c3131392c3134352c31332c3233322c3234332c34392c3232392c322c3234392c3230352c3230352c33392c3233352c34345d";
 pub const AGGREGATOR_ENDPOINT: &str =
@@ -28,26 +29,34 @@ extern "C" {
     fn log_many(a: &str, b: &str);
 }
 
-#[wasm_bindgen(start)]
-pub async fn main() -> Result<(), JsValue> {
-    let certificate_hash = "61b241a842ae986e54df26a43f5ebef2c6876d1b6fba5122cb20ca77df74131f";
-    let aggregator_endpoint = AGGREGATOR_ENDPOINT;
-    let genesis_verification_key = GENESIS_VERIFICATION_KEY;
+#[wasm_bindgen]
+pub async fn run(
+    aggregator_endpoint: &str,
+    certificate_hash: &str,
+    genesis_verification_key: &str,
+) -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    print_body(&window, "Verify certificate chain...");
 
     let chain_verification = verify_certificate_chain(
         aggregator_endpoint,
         certificate_hash,
         genesis_verification_key,
+        &window,
     )
     .await;
     match &chain_verification {
         Ok(_) => {
-            alert("✅ certificate chain verified!");
+            print_body(&window, "✅ certificate chain is verified!");
         }
         Err(e) => {
-            alert(&format!("❌ certificate chain invalid: {:?}", e));
+            print_body(
+                &window,
+                &format!("❌ certificate chain is invalid: {:?}", e),
+            );
         }
     }
+
     chain_verification?;
 
     Ok(())
@@ -57,6 +66,7 @@ pub async fn verify_certificate_chain(
     aggregator_endpoint: &str,
     certificate_hash: &str,
     genesis_verification_key: &str,
+    window: &Window,
 ) -> Result<Certificate, JsValue> {
     let genesis_verification_key: ed25519_dalek::VerifyingKey =
         key_decode_hex(genesis_verification_key)?;
@@ -70,7 +80,10 @@ pub async fn verify_certificate_chain(
                 certificate.hash
             )
         })?;
-        log_many("Certificate with hash='{}' is valid.", &certificate.hash);
+        print_body(
+            window,
+            &format!("Certificate with hash='{}' is valid.", &certificate.hash),
+        );
 
         certificate = match &certificate.previous_hash {
             None => break,
@@ -79,4 +92,14 @@ pub async fn verify_certificate_chain(
     }
 
     Ok(certificate)
+}
+
+fn print_body(window: &Window, message: &str) -> Result<(), JsValue> {
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+    let val = document.create_element("div")?;
+    body.append_child(&val)?;
+    val.set_text_content(Some(message));
+
+    Ok(())
 }
